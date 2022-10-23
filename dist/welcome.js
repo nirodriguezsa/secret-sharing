3471b8e9bec62c133a84db7df13dc9291d72b0c6
Nicolas Rodriguez <nicolas.salguero@rappi.com>
2022-10-23 01:43:38 -0500

    Init commit, app runs

---
+++ dist/welcome.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.welcome = void 0;
+function welcome() {
+    console.log('\x1b[36m', `
+    ███╗░░██╗██████╗░░██████╗
+    ████╗░██║██╔══██╗██╔════╝
+    ██╔██╗██║██████╔╝╚█████╗░
+    ██║╚████║██╔══██╗░╚═══██╗
+    ██║░╚███║██║░░██║██████╔╝
+    ╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░
+      Welcome to ${process.env.X_APPLICATION_ID}`, '\x1b[0m');
+}
+exports.welcome = welcome;
+//# sourceMappingURL=welcome.js.map
